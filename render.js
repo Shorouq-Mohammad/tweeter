38cf3d49449f6fe5e3fe8ed2af7e4e3dc7f37cce
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-11-05 16:36:29 +0200

    tweeter controller

--- render.js
+++ render.js
@@ -3,14 +3,22 @@ const Renderer = function(){
     const renderPosts = function(posts){
         $("#posts").empty()
         for(let post of posts){
-            let str= `<div class="post" data-id="${post.id}"><p class="post-text">${post.text}</p><ul class="comments">${renderComments(post.comments)}</ul></div>`
+            let str= `<div class="post" data-id="${post.id}">
+            <p class="post-text">${post.text}</p>
+            <div class="comments">${renderComments(post.comments)}<br>
+            <input type="text" placeholder="Got something to say?" class="input">
+            <button class="comment">Comment</button></div><br>
+            <button class="delete">Delete Post</button>
+            </div>`
             $("#posts").append(str)
         }
     }
     const renderComments = function(comments){
         let str = ""
         for(let comment of comments){
-            str += `<li data-id="${comment.id}">${comment.text}</li>`
+            str += `<div data-id="${comment.id}">
+            <span class="delete-comment">X</span>
+            <span> ${comment.text}</span></div>`
         }
         return str
     }
